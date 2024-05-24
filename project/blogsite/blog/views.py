fc217be786cb52df34f828c84dc921b725207d0c
Kashmin Shrestha <kashmin44@gmail.com>
2024-05-24 09:09:44 +0545

    image

--- project/blogsite/blog/views.py
+++ project/blogsite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 # Create your views here.
+
+def index(request):
+    blogs = Blog.objects.all()
+    return render(request, 'index.html', {'blogs': blogs})
